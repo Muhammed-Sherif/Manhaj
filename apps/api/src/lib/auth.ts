8e0c2bcda1792d724dea7e135ce69cca9b1e72de
mohamed sherif <muhammed.shereaf@gmail.com>
2026-09-14 13:48:05 +0300

    FIX

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -7,7 +7,7 @@ import bcrypt from 'bcryptjs';
 import {
   getDb,
 } from '@manhaj/db';
-import { betterAuthUser, betterAuthAccount, betterAuthSession, betterAuthVerification } from '@manhaj/db/schema';
+import { betterAuthUser, betterAuthAccount, betterAuthSession, betterAuthVerification } from '@manhaj/db';
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL  || 'https://manhaj-api-five.vercel.app',
